9d20990d9d666f01bcebf8c68e27d67b04fefc46
fbmcarmo <fbrunomcarmo@gmail.com>
2025-06-11 16:27:52 -0300

    Novas alteracoes

--- src/routes/livros.js
+++ src/routes/livros.js
@@ -2,22 +2,22 @@ const express = require('express')
 const router = express.Router()
 const livrosController = require('../controllers/livros')
 const livrosMiddleware = require('../middlewares/livros')
-const authMiddleware = require('../middlewares/auth')
+//const authMiddleware = require('../middlewares/auth')
 
 router.get(
     '/livros', 
-    authMiddleware.validateToken,
+    //authMiddleware.validateToken,
     livrosController.getLivros
 )
 router.post(
     '/livros',
-    authMiddleware.validateToken,
+    //authMiddleware.validateToken,
     livrosMiddleware.validateCreateLivro,
     livrosController.createLivro
 )
 router.delete(
     '/livros/:id',
-    authMiddleware.validateToken,
+    //authMiddleware.validateToken,
     livrosMiddleware.validateDeleteLivro,
     livrosController.deleteLivro
 )
