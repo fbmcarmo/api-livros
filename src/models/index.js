27cbf225bf34bf302c2e0f553ce3eeb36f9b9086
fbmcarmo <fbrunomcarmo@gmail.com>
2025-06-11 16:06:27 -0300

    Adicionei index.js no models

---
+++ src/models/index.js
@@ -0,0 +1,14 @@
+const sequelize = require('../config/database')
+const Users = require('./users')
+const Livros = require('./livros')
+
+sequelize.sync({alter: true})
+    .then(() => console.log('Tabelas sincronizadas com sucesso'))
+    .catch((error) => console.error(
+        'Erro ao sincronizar tabelas', error
+    ))
+
+    module.exports = {
+        Users,
+        Livros
+    }
